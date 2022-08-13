019f7ad881efcae141750ec909cf2619325e5d98
David Razmadze <razd45@gmail.com>
2022-08-13 12:57:29 -0400

    Initial Commit

---
+++ MathExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathExample
+//
+//  Created by David Razmadze on 8/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
